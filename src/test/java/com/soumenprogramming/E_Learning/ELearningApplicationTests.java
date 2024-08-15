edfd2649b2a1ba646e092641766b4b0ca22ba996
soumenprogramming <mail2soumen007@gmail.com>
2024-08-15 16:09:46 +0530

    first commit

---
+++ src/test/java/com/soumenprogramming/E_Learning/ELearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soumenprogramming.E_Learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
