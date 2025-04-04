3654e8a5fba6735e2e50dd3bd961e5dccd5eb239
soumen-trustt <soumen.manna@trustt.com>
2025-04-04 14:43:50 +0530

    login issue resolved and some features added

--- src/main/java/com/soumenprogramming/elearning/Application.java
+++ src/main/java/com/soumenprogramming/elearning/Application.java
@@ -3,7 +3,10 @@ package com.soumenprogramming.elearning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.soumenprogramming.elearning")
@@ -11,5 +14,18 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*");
+			}
+		};
 	}
 }
